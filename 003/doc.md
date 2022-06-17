fc7573f91cfa7b0932d179cda25f3fa8a9b23936
sudeshsubedi <programmedevil13@gmail.com>
2022-06-17 20:16:54 +0545

    Initial commit

---
+++ 003/doc.md
@@ -0,0 +1,8 @@
+# To be familiar with selective structure (branching)
+
+## OBJECTIVES
+    1. TODO
+    2. TODO
+
+### WAP to check whether a number is negative, positive or zero
+
---
+++ 003/program/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if(n < 0)
+        printf("The number is negative\n");
+    else if(n == 0)
+        printf("The number is zero\n");
+    else
+        printf("The number is positive\n");
+    return 0;
+}
